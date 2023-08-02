6288dfd6660e298388ec2a5d3a1bc7c85685b1f6
SLYVYNET <sofiialyvynets@otto.de>
2023-08-02 14:27:43 +0200

    PalindromeChecher program and Test for it

---
+++ src/test/java/my/palindrome/PalindromeCheckerTest.java
@@ -0,0 +1,26 @@
+package my.palindrome;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PalindromeCheckerTest {
+    @Test
+    public void testIsPalindrome() {
+        boolean test1 = PalindromeChecker.isPalindrome("Tenet");
+        boolean test2 = PalindromeChecker.isPalindrome("A man, a plan, a canal, Panama!");
+        boolean test3 = PalindromeChecker.isPalindrome("racecar");
+        boolean test4 = PalindromeChecker.isPalindrome("not a palindrome");
+
+        System.out.println("Test 1 - Is 'Tenet' a palindrome? " + test1);
+        System.out.println("Test 2 - Is 'A man, a plan, a canal, Panama!' a palindrome? " + test2);
+        System.out.println("Test 3 - Is 'racecar' a palindrome? " + test3);
+        System.out.println("Test 4 - Is 'not a palindrome' a palindrome? " + test4);
+
+        assertTrue(test1);
+        assertTrue(test2);
+        assertFalse(test3);
+        assertFalse(test4);
+    }
+}
