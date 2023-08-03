be53c0cfd32aef2abed8cb1ff86636c15b080977
SLYVYNET <sofiialyvynets@otto.de>
2023-08-03 09:36:04 +0200

    Calculator class

---
+++ src/main/java/my/calculator/Calculator.java
@@ -0,0 +1,32 @@
+package my.calculator;
+public class Calculator {
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double divide(double num1, double num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return num1 / num2;
+    }
+
+    public double sqrt(double num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Cannot calculate the square root of a negative number!");
+        }
+        return Math.sqrt(num);
+    }
+
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+}
